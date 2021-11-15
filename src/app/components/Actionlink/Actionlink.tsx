7f1cf7d3543fad623cef9213b99dd639ba6eabf8
Samet Uslu <samet_u@hotmail.de>
2021-11-15 19:30:53 +0100

    add button and icon

--- src/app/components/Actionlink/Actionlink.tsx
+++ src/app/components/Actionlink/Actionlink.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import ActionIcon from '../Icons/ActionIcon';
 
 type ActionlinProps = {
   url: string;
@@ -19,35 +20,7 @@ function ActionLink({ url, text }: ActionlinProps): JSX.Element {
       {text}
       {!hidden && (
         <Icon>
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-          >
-            <rect width="24" height="24" fill="url(#pattern0)" />
-            <defs>
-              <pattern
-                id="pattern0"
-                patternContentUnits="objectBoundingBox"
-                width="1"
-                height="1"
-              >
-                <use
-                  xlinkHref="#image0_219_294"
-                  transform="scale(0.0333333)"
-                />
-              </pattern>
-              <image
-                id="image0_219_294"
-                width="30"
-                height="30"
-                xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAcklEQVRIie3WMQ6CUBAG4S/cQYL3v4mNCVJh4XGgwIrIC+SpifGfdorZbLFZwj9zwYD2oKumx4T7i0DJVXPC+Aw8cN7pEk888d+Jt5YDMuF2wIHm3dN8mvU6u50u0UQT/V6U5eJsvTclV83V9kNXciGAGVM9cX2eizuGAAAAAElFTkSuQmCC"
-              />
-            </defs>
-          </svg>
+          <ActionIcon />
         </Icon>
       )}
     </Actionlink>
@@ -62,11 +35,13 @@ const Actionlink = styled.a`
   border-radius: 5px;
   padding: 13px 105px;
   text-decoration: none;
-  position: relative;
+  margin: 20px;
+  display: flex;
+  justify-content: center;
 `;
 
 const Icon = styled.span`
   position: absolute;
-  right: 10px;
-  top: 22%;
+  align-self: center;
+  right: 50px;
 `;
