6d8480251cef89fa5203fe5036ea5d9959cacdbc
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-17 12:36:12 +0100

    Add tagList logic

--- src/app/components/TagList/TagList.tsx
+++ src/app/components/TagList/TagList.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import Tag from '../Tag/Tag';
 
+type TagProps = {
+  id: number;
+  genreName: string;
+  active?: boolean;
+  count?: string;
+  onClick: () => void;
+};
+
 type TagListProps = {
-  tagList: string[];
+  tagList: TagProps[];
 };
 
 export default function TagList({ tagList }: TagListProps): JSX.Element {
   return (
     <div>
-      <Tag active>All</Tag>
       {tagList.map((tag) => (
-        <Tag>{tag}</Tag>
+        <Tag
+          key={tag.id}
+          id={tag.id}
+          active={tag.active}
+          onClick={tag.onClick}
+          count={tag.count}
+        >
+          {tag.genreName}
+        </Tag>
       ))}
     </div>
   );
